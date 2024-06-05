973336ab1fb719ffd927c52cae5b2e60a1b0e957
anasfalah3 <anasfalah3@gmail.com>
2024-06-05 15:26:00 +0100

    migrations

---
+++ database/migrations/2024_06_05_135203_create_podcasts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('podcasts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title');
+            $table->string('description');
+            $table->string('file_path');
+            $table->string('cover_path');
+            $table->foreignId('categorie_id')->constrained();
+            $table->string('tags');
+            $table->string('status');
+            $table->integer('views');
+            $table->integer('downloads');
+            $table->integer('duration');
+            $table->string('language');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('podcasts');
+    }
+};
