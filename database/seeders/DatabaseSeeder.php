f53391da4ce03251085b9d84085cc4b86f399192
anasfalah3 <anasfalah3@gmail.com>
2024-06-07 21:58:27 +0100

    update user table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'firstName' => 'Anas',
+            'lastName' => 'Falah',
+            'email' => 'anasfalah3@gmail.com',
+            'password' => '12345678',
         ]);
     }
 }
